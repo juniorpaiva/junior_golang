caa4b530fd7c11cea8cf187ea1cfe1116263dc63
JuniorPaiva <edvaldojuniorinfo@gmail.com>
2023-08-22 13:25:09 -0300

    add tipagem

---
+++ values_type.go
@@ -0,0 +1,17 @@
+// variables_types.go
+package main
+
+import "fmt"
+
+func main() {
+	//Concatena as duas strings com o sinal de +
+
+	fmt.Println("go" + "lang") //Saída: golang
+
+	fmt.Println("1 + 1 = ", 1+1)         // Saída: 1 + 1 = 2
+	fmt.Println("7.0 / 3.0 = ", 7.0/3.0) //Saída: 7.0 / 3.0 = 2.3333333333333335
+
+	fmt.Println(true && false) //Saída: false
+	fmt.Println(true || false) //Saída: true
+	fmt.Println(!true)         //Saída: false
+}
