c5bfeaa457ebf6883eda0656d815d9a31a01b7ca
JuniorPaiva <edvaldojuniorinfo@gmail.com>
2023-08-22 14:01:55 -0300

    Adicionando readme

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Aprendendo GOLANG
+
+[Video-aulas GO:](https://www.youtube.com/watch?v=bXRIGDn9tDY&list=PLeFetwYAi-F-h1RyywJAysOy74nvUs6i-&index=4)
+
+## Módulos
+
+### **fmt**: 
+<sup>Imprimir valores  ou strings na tela</sup>
+### reflect: Imprimir o tipo da variável declarada
+
+## Comandos Básicos
+
+go run codigo.go
--- values_type.go
+++ values_type.go
@@ -6,7 +6,7 @@ import "fmt"
 func main() {
 	//Concatena as duas strings com o sinal de +
 
-	fmt.Println("go" + "lang") //Saída: golang
+	fmt.Println("go + lang = ", "go"+"lang") //Saída: golang
 
 	fmt.Println("1 + 1 = ", 1+1)         // Saída: 1 + 1 = 2
 	fmt.Println("7.0 / 3.0 = ", 7.0/3.0) //Saída: 7.0 / 3.0 = 2.3333333333333335
